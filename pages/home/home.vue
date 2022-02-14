5803a69a684c9ffbbbb020abe96d32e38fdb765b
youhess <1176305338@qq.com>
2022-02-14 21:30:31 +0800

    完成了tabbar的开发

---
+++ pages/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		this is home page
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>

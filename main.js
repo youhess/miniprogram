19a7899dd897b0a729da0c359d9d33b3e62373d8
youhess <1176305338@qq.com>
2022-02-15 16:03:19 +0800

    完成了home首页开发

--- main.js
+++ main.js
@@ -2,6 +2,33 @@ import App from './App'
 
 // #ifndef VUE3
 import Vue from 'vue'
+
+
+// 导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+// 配置请求的跟路径
+$http.baseUrl = 'https://www.uinav.com'
+
+$http.beforeRequest = function(options){
+	uni.showLoading({
+		title:'数据加载中...'
+	})
+}
+$http.afterRequest = function(){
+	uni.hideLoading()
+}
+// 封装弹框的方法
+
+uni.$showMsg = function(title = '数据加载失败',duration = 1500){
+	uni.showToast({
+		title,
+		duration,
+		icon:'none'
+	})
+}
+
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,42 @@
 <template>
 	<view>
-		this is home page
+		<!-- 轮播图区域 -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<swiper-item v-for="(item,index) in swiperList" :key = "index" >
+				<navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id">
+					<image :src="item.image_src" ></image>
+				</navigator>
+			</swiper-item>
+
+		</swiper>
+		<!-- 分区ui结构 -->
+		<view class="nav-list">
+			<view class="nav-item" v-for="(item,index) in navList" :key = "index" @click="navClickHandler(item)">
+					<image class="nav-img" :src="item.image_src" mode=""></image>
+			</view>
+		</view>
+		<!-- 楼层区域 -->
+		<view class="floor-list">
+		  <!-- 楼层 item 项 -->
+		  <view class="floor-item" v-for="(item, i) in floorList" :key="i">
+		    <!-- 楼层标题 -->
+		    <image :src="item.floor_title.image_src" class="floor-title"></image>
+			<!-- 楼层图片区域 -->
+		<view class="floor-img-box">
+		  <!-- 左侧大图片的盒子 -->
+		  <navigator class="left-img-box" :url="item.product_list[0].url">
+		    <image :src="item.product_list[0].image_src" :style="{width: item.product_list[0].image_width + 'rpx'}" mode="widthFix"></image>
+		  </navigator>
+		  <!-- 右侧 4 个小图片的盒子 -->
+		  <view class="right-img-box">
+		    <navigator class="right-img-item" v-for="(item2, i2) in item.product_list" :key="i2" v-if="i2 !== 0" :url="item2.url">
+		      <image :src="item2.image_src" mode="widthFix" :style="{width: item2.image_width + 'rpx'}"></image>
+		    </navigator>
+		  </view>
+		</view>
+		  </view>
+		</view>
+		
 	</view>
 </template>
 
@@ -8,12 +44,97 @@
 	export default {
 		data() {
 			return {
-				
+				// 轮播数据
+				swiperList:[],
+				navList:[],
+				//  楼层的数据列表
+				floorList: [],
 			};
+		},
+		onLoad(){
+			// 在onload下调用获取轮播图数组的方法
+			this.getSwiperList(),
+			this.getNavList(),
+			// 在 onLoad 中调用获取楼层数据的方法
+			this.getFloorList()
+		},
+		methods:{
+			async getSwiperList(){
+				// 发起请求
+				const {data:res} = await uni.$http.get('/api/public/v1/home/swiperdata')
+				// 请求失败
+
+				if(res.meta.status !== 200){
+					return uni.$showMsg()
+				}
+				return this.swiperList = res.message
+			},
+			async getNavList(){
+				const {data:res} = await uni.$http.get('/api/public/v1/home/catitems');
+				if(res.meta.status !== 200) return uni.$showMsg()
+
+				return this.navList = res.message
+			},
+			async getFloorList() {
+			    const { data: res } = await uni.$http.get('/api/public/v1/home/floordata')
+			    if (res.meta.status !== 200) return uni.$showMsg()
+				console.log(res)
+				
+				// 通过双层 forEach 循环，处理 URL 地址
+				  res.message.forEach(floor => {
+				    floor.product_list.forEach(prod => {
+				      prod.url = '/subpkg/goods_list/goods_list?' + prod.navigator_url.split('?')[1]
+				    })
+				  })
+				  
+				return this.floorList = res.message
+			 },
+			// nav-item 项被点击时候的事件处理函数
+			navClickHandler(item) {
+			  // 判断点击的是哪个 nav
+			  if (item.name === '分类') {
+			    uni.switchTab({
+			      url: '/pages/cate/cate'
+			    })
+			  }
+			}
+			
 		}
 	}
 </script>
 
 <style lang="scss">
-
+	swiper{
+		height: 330rpx;
+		.swiper-item,image{
+			width: 100%;
+			height: 100%;
+		}
+	}
+	
+	.nav-list {
+	  display: flex;
+	  justify-content: space-around;
+	  margin: 15px 0;
+	
+	  .nav-img {
+	    width: 128rpx;
+	    height: 140rpx;
+	  }
+	}
+	.floor-title {
+	  height: 60rpx;
+	  width: 100%;
+	  display: flex;
+	}
+	.floor-img-box{
+		 display: flex;
+		 padding-left: 10rpx;
+	}
+	.right-img-box{
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
+	}
+	
 </style>

abfebd7058fccce90f4eda4644cf7be816cff1a5
youhess <1176305338@qq.com>
2022-02-16 16:18:33 +0800

    完成列表页面的开发

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
 	<view>
-		
+		<view class="goods-list">
+			<view v-for="(goods, i) in goodsList" :key="i" @click="gotoDetail(goods)">
+				<mygoods :goods="goods"></mygoods>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,9 +12,78 @@
 	export default {
 		data() {
 			return {
-				
+				// 请求参数对象
+				queryObj: {
+					// 查询关键词
+					query: '',
+					// 商品分类Id
+					cid: '',
+					// 页码值
+					pagenum: 1,
+					// 每页显示多少条数据
+					pagesize: 10
+				},
+				// 商品列表的数据
+				goodsList: [],
+				// 总数量，用来实现分页
+				total: 0,
+				// 是否正在请求数据
+				isloading: false
+
 			};
+		},
+		onLoad(options) {
+			// 将页面参数转存到 this.queryObj 对象中
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+			// 调用获取商品列表数据的方法
+			this.getGoodsList()
+		},
+		methods: {
+			// 获取商品列表数据的方法
+			async getGoodsList(cb) {
+				this.isloading = true
+		 	// 发起请求
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				this.isloading = false
+				cb&&cb()
+				if (res.meta.status !== 200) return uni.$showMsg()
+				// 为数据赋值
+				// 为数据赋值：通过展开运算符的形式，进行新旧数据的拼接
+				this.goodsList = [...this.goodsList, ...res.message.goods]
+				this.total = res.message.total
+				
+			},
+			gotoDetail(goods){
+				uni.navigateTo({
+				    url: '/subpkg/goods_detail/goods_detail?goods_id=' + goods.goods_id
+				  })
+			}
+
+		},
+		// 	触底事件
+		onReachBottom() {
+			if (this.isloading) return
+			// 判断是否还有下一页数据
+			if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕')
+			this.queryObj.pagenum += 1;
+			// 重新获取数据列表
+			this.getGoodsList();
+		},
+		 // 下拉刷新的事件
+		onPullDownRefresh() {
+			// 1. 重置关键数据
+			this.queryObj.pagenum = 1
+			this.total = 0
+			this.isloading = false
+			this.goodsList = []
+
+			// 2. 重新发起请求
+			this.getGoodsList(() => uni.stopPullDownRefresh())
 		}
+
 	}
 </script>
 
